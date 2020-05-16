1c7707503a617dee18a8ea46243e939fb5a238e6
Ose Djan <osedjan@Oses-MBP.fios-router.home>
2020-05-16 18:49:25 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -2,32 +2,39 @@
 var tableData = data;
 
 // select tbody using d3
-tbody = d3.select("tbody")
+tbody = d3.select("tbody");
 
 // Get into key and value pairs of data inside of the table
 // and loop through to add to the table in html
 function displayData(data){ 
-    tbody.text("")
-    data.forEach(function(sighting){
-    new_tr = tbody.append("tr")
-    Object.entries(sighting).forEach(function([key, value]){
-        new_td = new_tr.append("td").text(value)	
-    })
-})}
+    tbody.html("");
+    data.forEach((sighting)=>{
+    var new_tr = tbody.append("tr");
+    Object.values(sighting).forEach((value)=>{
+        var new_td = new_tr.append("td").text(value);	
+    });
+});}
 
-displayData(tableData)
 
-var inputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
+
+
+// var button = d3.select("#filter-btn")
 
 // Filter Data
 function changeHandler(){
-    d3.event.preventDefault();
-    console.log(inputText.property("value"));
-    var new_table = tableData.filter(sighting => sighting.datetime===inputText.property("value"))
-    displayData(new_table)
+    let new_table = tableData;
+    // d3.event.preventDefault();
+    // console.log(inputText.property("value"));
+    var inputText = d3.select("#datetime").property("value");
+    if (inputText){
+    new_table = new_table.filter(new_tr => new_tr.datetime===inputText);
+}
+    displayData(new_table);
 }
 
 // Change and click
-inputText.on("change", changeHandler)
-button.on("click", changeHandler)
+// inputText.on("change", changeHandler)
+// button.on("click", changeHandler)
+
+d3.selectAll("#filter-btn").on("click",changeHandler);
+displayData(tableData)
